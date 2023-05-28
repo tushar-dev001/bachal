eaea6aa6b32c0efe6fa1d2c8681a5eb09e008f14
tushari789 <tushar789imran@gmail.com>
2023-05-29 00:26:49 +0600

    firebase authentication added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,66 @@
-import './Login.css'
+import "./Login.css";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
-import Heading from '../../components/Heading';
-import loginImg from '../../../public/loginImg.png'
-import google from '../../../public/google.png'
+import Heading from "../../components/Heading";
+import loginImg from "../../../public/loginImg.png";
+import google from "../../../public/google.png";
+
+import { useState } from "react";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useNavigate, Link } from "react-router-dom";
+
+const initialValue = {
+  email: "",
+  fullName: "",
+  password: "",
+  // loading: ''
+};
 
 const Login = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  // const navigate = useNavigate()
+
+  const [values, setValues] = useState(initialValue);
+  const [loader, setLoader] = useState();
+
+  const handleValues = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSignUp = () => {
+    const { email, password } = values;
+    setLoader(true);
+
+    signInWithEmailAndPassword(auth, email, password).then((user) => {
+      console.log(user);
+      
+      setValues({
+        ...values,
+        email: "",
+        password: "",
+      });
+      setLoader(false);
+
+      // navigate('/login')
+    });
+  };
+
+  const handleGoogleLogin = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      console.log(result, "google sent");
+    });
+  };
+
   return (
     <>
       <Grid container spacing={2}>
@@ -13,7 +68,7 @@ const Login = () => {
           <div className="login-container">
             <Heading title="Login to your account!" />
 
-            <img className='google' src={google}/>
+            <img onClick={handleGoogleLogin} className="google" src={google} />
 
             <div className="reg-input">
               <TextField
@@ -21,6 +76,9 @@ const Login = () => {
                 label="Email Address"
                 variant="outlined"
                 type="email"
+                onChange={handleValues}
+                name="email"
+                value={values.email}
               />
             </div>
             <div className="reg-input">
@@ -29,10 +87,28 @@ const Login = () => {
                 label="Password"
                 variant="outlined"
                 type="password"
+                onChange={handleValues}
+                name="password"
+                value={values.password}
               />
             </div>
 
-            <button className="signUp-btn">Login to Continue</button>
+            {loader ? (
+              <LoadingButton className="signUp-btn" loading variant="outlined">
+                Submit
+              </LoadingButton>
+            ) : (
+              <button className="signUp-btn" onClick={handleSignUp}>
+                Login To Continue
+              </button>
+            )}
+
+            <p style={{ marginTop: "18px" }}>
+              Aren't have an account?{" "}
+              <strong style={{ color: "blue", cursor: "pointer" }}>
+                <Link to="/">Sign Up</Link>
+              </strong>
+            </p>
           </div>
         </Grid>
         <Grid h2 xs={6}>
--- src/pages/registration/Registration.jsx
+++ src/pages/registration/Registration.jsx
@@ -1,44 +1,157 @@
 import "./Registration.css";
-import TextField from "@mui/material/TextField";
-import Grid from "@mui/material/Grid";
+import { TextField, Grid, Alert } from "@mui/material";
 import regImg from "../../../public/regImg.png";
 import Heading from "../../components/Heading";
+import { useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { useNavigate, Link } from "react-router-dom";
+import {RiEyeFill, RiEyeCloseFill} from 'react-icons/ri'
+
+const initialValue = {
+  email: "",
+  fullName: "",
+  password: "",
+  // loading: 'false'
+  error:"",
+  eye: false,
+};
 
 const Registration = () => {
+  const auth = getAuth();
+  const navigate = useNavigate()
+
+  const [values, setValues] = useState(initialValue);
+  const [loader, setLoader] = useState()
+
+  const handleValues = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSignUp = () => {
+    const { email, fullName, password } = values;
+
+    if(!email){
+      setValues({
+            ...values,
+            error: "Enter Email Address",
+          })
+          return
+    }
+
+    if(!fullName){
+      setValues({
+            ...values,
+            error: "Enter Full Name",
+          })
+          return
+    }
+
+    if(!password){
+      setValues({
+            ...values,
+            error: "Enter Password",
+          })
+          return
+    }
+     
+    setLoader(true)
+    createUserWithEmailAndPassword(auth, email, password).then((user) => {
+      console.log(user);
+      sendEmailVerification(auth.currentUser).then(() => {
+        console.log("sent email");
+      });
+
+      setValues({
+        ...values,
+        email: "",
+        fullName: "",
+        password: "",
+        error: 'flase'
+      });
+      setLoader(false)
+
+      navigate('/login')
+    });
+  };
+
+  const handleEye = () =>{
+    console.log("one");
+  }
+
   return (
     <>
       <Grid container spacing={2}>
-        <Grid h2 xs={6}>
+        <Grid xs={6}>
           <div className="reg-container">
             <Heading title="Get started with easily register" />
             <p className="reg-para">Free register and you can enjoy it</p>
 
+
+            
             <div className="reg-input">
+              {values.error.includes("Email") && <Alert severity="error">{values.error}</Alert>}
               <TextField
                 id="outlined-basic"
                 label="Email Address"
                 variant="outlined"
                 type="email"
+                onChange={handleValues}
+                name="email"
               />
             </div>
+
             <div className="reg-input">
+            {values.error.includes('Name') && <Alert severity="error">{values.error}</Alert>}
               <TextField
                 id="outlined-basic"
                 label="Full Name"
                 variant="outlined"
                 type="name"
+                onChange={handleValues}
+                name="fullName"
               />
             </div>
             <div className="reg-input">
+            {values.error.includes('Password') && <Alert severity="error">{values.error}</Alert>}
               <TextField
                 id="outlined-basic"
                 label="Password"
                 variant="outlined"
-                type="password"
+                type= {values.eye? "text": "password"}
+                onChange={handleValues}
+                name="password"
               />
+
+              <div className="eye" onClick={()=>setValues({...values, eye:!values.eye})}>
+                {values.eye 
+                ?
+                <RiEyeFill />
+                :
+                <RiEyeCloseFill/>
+                }
+              </div>
             </div>
 
-            <button className="signUp-btn">Sing Up</button>
+          {loader
+          ?
+            <LoadingButton className="signUp-btn" loading variant="outlined">
+              Submit
+            </LoadingButton>
+          :
+            <button className="signUp-btn" onClick={handleSignUp}>
+              Sing Up
+            </button>
+          }
+            <p style={{ marginTop: "18px" }}>
+              Already have an account?{" "}
+              <strong style={{ color: "blue", cursor: "pointer" }}>
+                <Link to='/login'>Login</Link>
+              </strong>
+            </p>
           </div>
         </Grid>
         <Grid h2 xs={6}>
